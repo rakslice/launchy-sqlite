2b929fe60cdb4c479ec651e4c0998411b5cf9ec3
Andrew Tonner <rakslice@gmail.com>
2018-01-02 21:10:04 -0800

    look for space-separated words anywhere in any order; use space-separated words that appear in any action name to filter actions first, (fixes #3) ; add support for multiple actions for a column -- use a list of action objects in place of a single object; add constants for development debug output and timing; cleanup

--- sqlite.py
+++ sqlite.py
@@ -11,9 +11,17 @@ import sqlite3
 import subprocess
 
 import sys
+import time
 
 from PyQt4 import QtGui
 import ConfigFrame
+
+
+# prefix to prepend to the json in the full path for disambiguation
+PRE_TAG = ""
+
+DEBUG_OUTPUT = False
+DO_TIMING = False
 
 
 def read_json(filename):
@@ -60,22 +68,28 @@ class LaunchySQLite(launchy.Plugin):
     def init(self):
         self.conn = sqlite3.connect(self.database_filename)
 
+    # noinspection PyPep8Naming
     def getID(self):
         return self.hash
 
+    # noinspection PyPep8Naming
     def getName(self):
         return self.name
 
+    # noinspection PyPep8Naming
     def getIcon(self):
         return self.icon
 
+    # noinspection PyPep8Naming
     def getLabels(self, inputDataList):
         pass
 
+    # noinspection PyMethodMayBeStatic,PyPep8Naming
     def hasDialog(self):
         print "hasDialog()"
         return True
 
+    # noinspection PyPep8Naming
     def doDialog(self, parentWidgetPtr):
         print "doDialog()"
         parentWidget = wrapinstance(parentWidgetPtr, QtGui.QWidget)
@@ -85,6 +99,7 @@ class LaunchySQLite(launchy.Plugin):
 
         return unwrapinstance(self.widget)
 
+    # noinspection PyPep8Naming
     def endDialog(self, accept):
         pass
 
@@ -125,8 +140,10 @@ class LaunchySQLite(launchy.Plugin):
             print repr(args)
             subprocess.check_call(args, cwd=wd)
 
-    @staticmethod
-    def has_valid_action(field_def):
+    @classmethod
+    def has_valid_action(cls, field_def):
+        if type(field_def) == list:
+            return any(cls.has_valid_action(x) for x in field_def)
         if "action_url_pattern" in field_def:
             return True
         return False
@@ -143,21 +160,56 @@ class LaunchySQLite(launchy.Plugin):
                 url = action_url_pattern.replace("%s", urllib.quote(field_value))
             webbrowser.open(url)
 
+    @staticmethod
+    def get_action_name(field_def, output_column_name):
+        if field_def is None:
+            return None
+        action_name = field_def.get("action_name")
+        if action_name is None:
+            action_name = "Open %s" % output_column_name
+        return action_name
+
+    @staticmethod
+    def action_name_match(text_part, action_name):
+        return action_name is not None and text_part.lower() in action_name.lower()
+
+    @staticmethod
+    def field_defs_entry_proper(field_defs_entry):
+        if field_defs_entry is not None and type(field_defs_entry) != list:
+            return [field_defs_entry]
+        return field_defs_entry
+
+    # noinspection PyPep8Naming
     def getResults(self, inputDataList, resultsList):
         # Take the text from the first input item and add a new
         # Catalog item with our plugin id
 
         # print len(inputDataList)
-        text = inputDataList[0].getText()
+        inputData = inputDataList[0]
+
+        first_result = True
+
+        all_text = inputData.getText()
+
+        all_text_parts = all_text.split(" ")
+
+        if DO_TIMING:
+            start_time = time.time()
 
         # print inputDataList
 
         for table_entry in self.config["tables"]:
+            # go through the tables to use from this sqlite database according to the config
             table_name = table_entry["name"]
             display_name_field = table_entry.get("display_name_field")
             search_fields = table_entry["search_fields"]
+
+            # 1. build the list of fields we want for this table
             output_fields = []
+
+            # the fields definitions for this table from our config file
             field_defs = table_entry["fields"]
+
             for field_name, field_def in field_defs.iteritems():
                 if self.has_valid_action(field_def):
                     output_fields.append(field_name)
@@ -170,20 +222,87 @@ class LaunchySQLite(launchy.Plugin):
             if display_name_field and display_name_field not in output_fields:
                 output_fields.append(display_name_field)
 
+            # What we want
+            # - the rows where all the words are in any of the fields
+            # - for any combination of the words that are in an action, the rows where all the other words are in any of the fields
+            #   (we will show just certain actions for these)
+            #
+            # For a simple first cut of this, let's just narrow the actions to any word that is part of an action
+            # rather than doing the full union.
+
+            # 2. figure out what action names the search terms match and what other actions to exclude
+
+            action_names = []
+            action_entries = []
+
+            for output_column_name in output_fields:
+                field_defs_entry = self.field_defs_entry_proper(field_defs.get(output_column_name))
+                if field_defs_entry is None:
+                    continue
+                for field_def_num, field_def in enumerate(field_defs_entry):
+                    action_name = self.get_action_name(field_def, output_column_name)
+                    action_names.append(action_name)
+                    action_entries.append((output_column_name, field_def_num, action_name))
+
+            exclude_output_fields = set()
+
+            text_parts = []
+            for text_part in all_text_parts:
+                if any(self.action_name_match(text_part, action_name) for action_name in action_names):
+                    # text is in any action name; treat as a search for those specific actions
+
+                    # we only want actions that have all of these action words
+                    for output_field, field_def_num, action_name in action_entries:
+                        if action_name is None:
+                            continue
+                        if not self.action_name_match(text_part, action_name):
+                            key = (output_field, field_def_num)
+                            if key not in exclude_output_fields:
+                                exclude_output_fields.add(key)
+                else:
+                    # text is not in any action name; treat as ordinary search text
+                    text_parts.append(text_part)
+
+            if DEBUG_OUTPUT:
+                print ""
+                print "QUERY %r" % all_text_parts
+                print "exclude output fields %r" % exclude_output_fields
+                print "text parts %r" % text_parts
+
+            # 3. build database query to do for this table to match on those fields
+
             output_clause = ", ".join(sqlite_escape(x) for x in output_fields)
-            search_clause = " OR ".join("%s LIKE ?" % sqlite_escape(x) for x in search_fields)
+            if len(text_parts) == 0:
+                search_clause = "1"
+            else:
+                search_clause = " OR ".join("%s LIKE ?" % sqlite_escape(x) for x in search_fields)
+                search_clause = " AND ".join(["(" + search_clause + ")"] * len(text_parts))
+
             query = "SELECT %s FROM %s WHERE %s" % (output_clause, sqlite_escape(table_name), search_clause)
-            args = ["%" + text.replace("%", "%%") + "%"] * len(search_fields)
+
+            args = []
+            for text_part in text_parts:
+                args += ["%" + text_part.replace("%", "%%") + "%"] * len(search_fields)
+
+            if DEBUG_OUTPUT:
+                print query
 
             # print "running query: " + query
             # print "with args: " + repr(args)
 
+            # 4. Use the database query results to create the action items, respecting the action filters
+
             c = self.conn.cursor()
             assert isinstance(c, sqlite3.Cursor)
             try:
-                c.execute(query, args)
+                try:
+                    c.execute(query, args)
+                except sqlite3.OperationalError:
+                    print "failing query was %r args %r" % (query, args)
+                    raise
                 result_rows = c.fetchall()
-                # print "got %d rows" % len(result_rows)
+                if DEBUG_OUTPUT:
+                    print "got %d rows" % len(result_rows)
                 for result_row in result_rows:
 
                     column_pairs = zip(output_fields, result_row)
@@ -203,61 +322,84 @@ class LaunchySQLite(launchy.Plugin):
                     except Exception:
                         print "error getting icon"
                         traceback.print_exc()
-                    print "icon is " + icon
+                    # print "icon is " + icon
 
                     for output_column_name, output_value in column_pairs:
-
                         if not output_value:
                             continue
 
-                        field_def = field_defs.get(output_column_name)
-                        if field_def is None:
+                        field_defs_entry = self.field_defs_entry_proper(field_defs.get(output_column_name))
+                        if field_defs_entry is None:
                             continue
 
-                        if not self.has_valid_action(field_def):
-                            continue
+                        for field_def_number, field_def in enumerate(field_defs_entry):
+                            if (output_column_name, field_def_number) in exclude_output_fields:
+                                continue
 
-                        action_name = field_def.get("action_name")
-                        if action_name is None:
-                            action_name = "Open %s" % output_column_name
+                            if not self.has_valid_action(field_def):
+                                continue
 
-                        action = {"table": table_name, "field": output_column_name, "value": output_value}
-                        action_json = json.dumps(action)
+                            action_name = self.get_action_name(field_def, output_column_name)
 
-                        if display_name_field and column_dict[display_name_field]:
-                            entry_display_text = "%s: %s (%s)" % (column_dict[display_name_field], action_name, output_value)
-                        else:
-                            entry_display_text = "%s: %s" % (output_value,  action_name)
+                            action = {"table": table_name, "field": output_column_name, "value": output_value, "field_def_number": field_def_number}
+                            action_json = json.dumps(action)
 
-                        resultsList.push_back(launchy.CatItem(action_json,
-                                                              entry_display_text,
-                                                              self.getID(), icon))
+                            if display_name_field and column_dict[display_name_field]:
+                                entry_display_text = "%s: %s (%s)" % (column_dict[display_name_field], action_name, output_value)
+                            else:
+                                entry_display_text = "%s: %s" % (output_value,  action_name)
+
+                            if DEBUG_OUTPUT:
+                                print "ENTRY: " + entry_display_text
+
+                            cat_item = launchy.CatItem(PRE_TAG + action_json,
+                                                       entry_display_text,
+                                                       self.getID(), icon)
+
+                            resultsList.push_back(cat_item)
+                            if first_result:
+                                first_result = False
+                                inputData.setTopResult(cat_item)
+
             finally:
                 c.close()
 
+        if DO_TIMING:
+            end_time = time.time()
+
+            # noinspection PyUnboundLocalVariable
+            print "elapsed time %0.0f ms" % ((end_time - start_time) * 1000)
+
+    # noinspection PyPep8Naming
     def getCatalog(self, resultsList):
         pass
 
+    # noinspection PyPep8Naming
     def launchItem(self, inputDataList, catItemOrig):
         # The user chose our catalog item, print it
-        catItem = inputDataList[-1].getTopResult()
-        print "I was asked to launch: ", catItem.fullPath
-        action = json.loads(catItem.fullPath)
+        cat_item = inputDataList[-1].getTopResult()
+        full_path = cat_item.fullPath
+        print "I was asked to launch: ", full_path
+
+        assert full_path.startswith(PRE_TAG)
+        full_path = full_path[len(PRE_TAG):]
+
+        action = json.loads(full_path)
 
         field_name = action["field"]
         field_value = action["value"]
+        field_def_number = action["field_def_number"]
 
         for table_entry in self.config["tables"]:
             table_name = table_entry["name"]
             if table_name == action["table"]:
                 field_defs = table_entry["fields"]
-                field_def = field_defs[field_name]
+                field_defs_entry = self.field_defs_entry_proper(field_defs[field_name])
+                field_def = field_defs_entry[field_def_number]
                 self.do_action(field_def, field_value)
                 break
         else:
             assert False
-
-
 
 
 launchy.registerPlugin(LaunchySQLite)
